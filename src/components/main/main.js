c7b5986fb96d9380fa59e82e0760db7d09efcdf3
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-07-10 13:48:18 -0700

    still cant get img to show up

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,29 +1,49 @@
 import React , { Component } from "react";
-
+import Img from "../img/dbz.gif"
 import "./style.css";
 import Data from "../imgdata/img.json"
+import FriendCard from "../Card/friendCard.js"
 
 console.log(Data.img);
 console.log(Data.id);
 console.log(Data);
+console.log(Img);
 
+// function shuffleFriends(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+//   return array;
+// };
 
-// const mappingFunction = Data => Data.img;
-
-// const img = Data.map(mappingFunction);
 
 class Main extends Component {
-  state = {
-  friendData: Data
+  constructor (props) {
+    super(props)
+  
+  this.state = {
+  Data
   };
 
-
+  }
 
 render(){
   return(
- <div >
- <img alt = "" scr = {this.state.friendData}/>
+ <div className = "container">
+   <img alt = "" src ={Img}/>
+   {/* <h1>{names}</h1> */}
+{this.state.Data.map((item)=>(
+  <FriendCard 
+  key = {item.id}
+  image= {item.image}
+  name = {item.name}
+  clicked = {item.clicked}
+  />
+ 
 
+
+))}
  </div>
   )
 }
