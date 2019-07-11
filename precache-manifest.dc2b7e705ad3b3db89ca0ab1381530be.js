4420e23a6a109af0484f7415e6ce62a33ffa523b
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-07-11 16:32:20 -0700

    Updates

---
+++ precache-manifest.dc2b7e705ad3b3db89ca0ab1381530be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef2c40135d6aa45f9e206801e280e38",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "cb792ac98792e0f17970",
+    "url": "/memory-game/static/css/main.1999b3d7.chunk.css"
+  },
+  {
+    "revision": "4e1eb7894eb0bc23b583",
+    "url": "/memory-game/static/js/2.3bcb0307.chunk.js"
+  },
+  {
+    "revision": "cb792ac98792e0f17970",
+    "url": "/memory-game/static/js/main.f28be99c.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "fed31b7cbd7cf4eba30aa0907b9e7a20",
+    "url": "/memory-game/static/media/bananas.fed31b7c.png"
+  }
+]);
